eae8e46c2a1b296a800701862a3a74255834e1e0
skyred <sky.red2212@gmail.com>
2019-01-04 21:08:59 +0100

    new update

--- blog/models.py
+++ blog/models.py
@@ -242,6 +242,7 @@ class insta_pics(models.Model):
 
 class tokens(models.Model):
 	token = models.CharField(max_length = 2000,default=None)
+	expiry = models.CharField(max_length = 1000,default="0")
 
 	def __str__(self):
 		return self.token
--- blog/views.py
+++ blog/views.py
@@ -12,6 +12,7 @@ import json
 import threading
 import asyncio
 import random
+from datetime import datetime
 
 
 
@@ -20,6 +21,15 @@ import random
 
 
 
+
+
+def clearing_tokens():
+    token_list = tokens.objects.all()
+    for token in token_list:
+        if int(token.expiry) < int(datetime.now().timestamp()):
+            token.delete()
+        else:
+            pass
 
 
 
@@ -31,14 +41,16 @@ def token_handler(request):
         action = request.GET.get("action",None)
         if action == "add":
             token = request.GET.get("token",None)
-            instance = tokens.objects.create(token = token)
+            instance = tokens.objects.create(token = token,expiry = str(int(datetime.now().timestamp())+90))
             instance.save()
-            print("token saved ")
-            print(token)
+            clearing_tokens()
+            #print("token saved ")
+            #print(token)
             return HttpResponse(json.dumps({
                 "status":"added"
                 }))
         else:
+            clearing_tokens()
             tokens_list = [x for x in tokens.objects.all()]
             if len(tokens_list) != 0:
                 index = tokens_list.index(random.choice(tokens_list))
